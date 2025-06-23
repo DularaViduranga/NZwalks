60624c7bacf77d792bdd577d35479a84c276c027
Dulara Viduranga <121147525+DularaViduranga@users.noreply.github.com>
2025-06-23 19:14:38 +0530

    Update ValidateModel.cs

--- NZWalks.API/CustomActionFilters/ValidateModel.cs
+++ NZWalks.API/CustomActionFilters/ValidateModel.cs
@@ -1,1 +1,19 @@
-﻿
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace NzWalks.API.CustomActionFilters
+{
+    public class ValidateModel : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            if (context.ModelState.IsValid == false)
+            {
+                context.Result = new BadRequestObjectResult(context.ModelState);
+            }
+
+            
+        }
+    }
+}
+
